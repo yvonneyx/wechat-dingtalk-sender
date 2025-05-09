9852687a008c14b176e397b93d2b963c57157359
yvonneyx <zhuyuxin0627@gmail.com>
2025-05-09 15:45:49 +0800

    在 constants.ts 中添加多个新的群组及其访问令牌，并将 constants.local.ts 添加到 .gitignore

--- constants.ts
+++ constants.ts
@@ -7,5 +7,12 @@ export const dingGroups = [
     accessToken: process.env.GROUP_G2_G6_DOC_ACCESS_TOKEN
   },
   { id: '131685001908', name: 'AntV OSCP 文档季开发者交流群', accessToken: process.env.TOKEN_131685001908 },
-  { id: '9760006963', name: 'AntV 内部服务群（答疑先看公告）', accessToken: process.env.TOKEN_9760006963 }
+  { id: '9760006963', name: 'AntV 内部服务群（答疑先看公告）', accessToken: process.env.TOKEN_9760006963 },
+  { id: '32183967', name: '蚂蚁集团 - AntV 支持群', accessToken: process.env.TOKEN_32183967 },
+  { id: 'group-g2-ant-design-charts', name: 'AntV G2和 Ant Design Charts 技术交流群', accessToken: process.env.TOKEN_group_g2_ant_design_charts },
+  { id: '82400003328', name: 'G6 图分析引擎交流群', accessToken: process.env.TOKEN_82400003328 },
+  { id: '30088652', name: 'G6 图可视化交流群', accessToken: process.env.TOKEN_30088652 },
+  { id: '127855003906', name: 'G6 社区开源先锋', accessToken: process.env.TOKEN_127855003906 },
+  { id: 'group-g6-open-source-leader', name: 'AntV 开源先锋', accessToken: process.env.TOKEN_group_g6_open_source_leader },
+  { id: '44841855', name: 'F2 问题答疑 & 技术支持', accessToken: process.env.TOKEN_44841855 }
 ];
